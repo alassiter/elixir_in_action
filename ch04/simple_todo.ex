e4addb211e594a0bc000b6f064de6be354297106
Anthony Lassiter <anthonylassiter@gmail.com>
2019-06-29 18:50:53 -0400

    Add ch04 and some work

---
+++ ch04/simple_todo.ex
@@ -0,0 +1,11 @@
+defmodule TodoList do
+  def new(), do: MultiDict.new()
+
+  def add_entry(list, date, title) do
+    MultiDict.add(list, date, title)
+  end
+
+  def entries(list, date) do
+    MultiDict.get(list, date)
+  end
+end
